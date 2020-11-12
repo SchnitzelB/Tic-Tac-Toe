7fc35f78696e755053fcee0926449fe1c27c1861
SchnitzelB <es971120@icloud.com>
2020-11-12 15:48:12 -0600

    Created layout using println.

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -1,22 +1,34 @@
 package tictactoe;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("X");
-        System.out.print("O");
-        System.out.print("X");
 
-        char[] tttinput = {'O', '_', 'O', 'X', 'X', 'O', '_', 'X', 'X'};
+        char[] tttinput = new char[9];
+        String tttAsString = Arrays.toString(tttinput);
+
+        System.out.println("Enter an X.");
+        String userInput = scanner.next();
+        for (userInput.equals()) {
+
+        }
+
+        //        char[] rowOne = {'O', '_', 'O'};
+//        String rowOneString = Arrays.toString(rowOne);
+//        char[] rowTwo = {'X', 'X', 'O'};
+//        char[] rowThree = {'_', 'x', 'x'};
 
 
 
         System.out.println("---------");
-        System.out.print("| ");
-        System.out.print(tttinput);
-        System.out.print(" |");
+        System.out.println("| " + tttAsString.charAt(0) + " " + tttAsString.charAt(1) + " " + tttAsString.charAt(2) + " |");
+        System.out.println("| " + tttAsString.charAt(3) + " " + tttAsString.charAt(4) + " " + tttAsString.charAt(5) + " |");
+        System.out.println("| " + tttAsString.charAt(6) + " " + tttAsString.charAt(7) + " " + tttAsString.charAt(8) + " |");
         System.out.println("---------");
+
 
     }
 }
