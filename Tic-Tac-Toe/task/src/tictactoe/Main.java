74572f8bf889911ef0daaa0ecb44cf20dc2eac2a
SchnitzelB <es971120@icloud.com>
2020-11-15 01:11:41 -0600

    Added char input functionality.

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -7,18 +7,30 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-       char[][] tttinput = new char[3][3];
+       char [][] tttinput = new char[3][3];
 
-        System.out.println();
-        tttinput[0][0] = scanner.next();
-        tttinput[0][1] = scanner.next();
-        tttinput[0][2] = scanner.next();
-        tttinput[1][0] = scanner.next();
-        tttinput[1][1] = scanner.next();
-        tttinput[1][2] = scanner.next();
-        tttinput[2][0] = scanner.next();
-        tttinput[2][1] = scanner.next();
-        tttinput[2][2] = scanner.next();
+        tttinput[0][0] = scanner.next().charAt(0);
+
+        tttinput[0][1] = scanner.next().charAt(0);
+
+        tttinput[0][2] = scanner.next().charAt(0);
+
+        tttinput[1][0] = scanner.next().charAt(0);
+
+        tttinput[1][1] = scanner.next().charAt(0);
+
+        tttinput[1][2] = scanner.next().charAt(0);
+
+        tttinput[2][0] = scanner.next().charAt(0);
+
+        tttinput[2][1] = scanner.next().charAt(0);
+
+        tttinput[2][2] = scanner.next().charAt(0);
+
+
+//        if ((tttinput.equals("O")) && (tttinput.equals("O")) && (tttinput.equals("_")) {
+//
+//        }
 
 
 //        char[] rowOne = {'O', '_', 'O'};
